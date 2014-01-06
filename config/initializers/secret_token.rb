1fab3b400fe834a8a5c8f8d008b97803bb9767db
arjunan.ashok <arjunan.ashok@gmail.com>
2014-01-06 11:34:31 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bromax::Application.config.secret_key_base = '98995cdf501d3ddf4f4dbd23ee8b0c555c3ca777a29cf1cc057ceb62726483b832b2b24363ce55e11bfdfd4719de69620ec972e0086bb491be0fa032923a6a26'
